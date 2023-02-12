057d838e8dee81c01b61128102b5374dbea522b0
devstar57 <web.devstar57@gmail.com>
2023-02-12 13:38:43 +0100

    add: video controller slider feature with play/stop button

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^get$', views.get, name='get'),
+    re_path(r'^get_range_data$', views.get_range_data, name='get_range_data'),
 ]
