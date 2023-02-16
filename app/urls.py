1d0078fbdb1895c567cd4cab305753fa568dd78a
devstar57 <web.devstar57@gmail.com>
2023-02-16 14:41:55 +0100

    feat: change backend api endpoint for online db

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^get_range_data$', views.get_range_data, name='get_range_data'),
+    re_path(r'^get_player_data$', views.get_player_data, name='get_player_data'),
 ]
